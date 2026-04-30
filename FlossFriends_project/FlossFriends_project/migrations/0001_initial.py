482f70b3bfce79f5234ad93ea3eaefd13cd1296d
Фёдорова Василиса8 <vasilisa.f300@gmail.com>
2026-04-30 14:10:21 +0300

    3 попытка

--- FlossFriends_project/FlossFriends_project/migrations/0001_initial.py
+++ FlossFriends_project/FlossFriends_project/migrations/0001_initial.py
@@ -40,6 +40,34 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
+            name='CustomUser',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
+                ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('email_verified', models.BooleanField(default=False)),
+                ('verification_token', models.UUIDField(default=uuid.uuid4, editable=False)),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+            ],
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+                'abstract': False,
+            },
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+        migrations.CreateModel(
             name='Pattern',
             fields=[
                 ('pattern_id', models.AutoField(primary_key=True, serialize=False)),
@@ -52,7 +80,7 @@ class Migration(migrations.Migration):
                 ('created_date', models.DateTimeField()),
                 ('updated_date', models.DateTimeField(blank=True, null=True)),
                 ('colors_count', models.IntegerField()),
-                ('user', models.ForeignKey('CustomUser', on_delete=models.CASCADE)),
+                ('user', models.ForeignKey(on_delete=models.CASCADE, to='FlossFriends_project.customuser')),
                 ('canvas', models.ForeignKey('Canvas', on_delete=models.CASCADE)),
             ],
             options={
@@ -112,7 +140,7 @@ class Migration(migrations.Migration):
             name='Threadinventory',
             fields=[
                 ('thread_inventory_id', models.AutoField(primary_key=True, serialize=False)),
-                ('user', models.ForeignKey('CustomUser', on_delete=models.CASCADE)),
+                ('user', models.ForeignKey(on_delete=models.CASCADE, to='FlossFriends_project.customuser')),
                 ('thread', models.ForeignKey('Thread', on_delete=models.CASCADE)),
                 ('skeins_count', models.IntegerField()),
             ],
@@ -120,33 +148,5 @@ class Migration(migrations.Migration):
                 'db_table': 'ThreadInventory',
                 'managed': True,
             },
-        ),
-        migrations.CreateModel(
-            name='CustomUser',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
-                ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
-                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
-                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
-                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('email', models.EmailField(max_length=254, unique=True)),
-                ('email_verified', models.BooleanField(default=False)),
-                ('verification_token', models.UUIDField(default=uuid.uuid4, editable=False)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
-            ],
-            options={
-                'verbose_name': 'user',
-                'verbose_name_plural': 'users',
-                'abstract': False,
-            },
-            managers=[
-                ('objects', django.contrib.auth.models.UserManager()),
-            ],
         ),
     ]
